9d7f2c040cfce75b6a87fd728a92b05059d789fa
Franco Miranda <francomiranda1901@gmail.com>
2020-09-22 16:47:51 -0300

    Modificados los campos del autor

--- src/main/java/com/github/francomiranda19/finalreality/model/character/player/CharacterClass.java
+++ src/main/java/com/github/francomiranda19/finalreality/model/character/player/CharacterClass.java
@@ -4,7 +4,7 @@ package com.github.francomiranda19.finalreality.model.character.player;
  * Enumeration of the classes a player character may have.
  *
  * @author Ignacio Slater Muñoz.
- * @author <Your name>
+ * @author Franco Miranda Oyarzún
  */
 public enum CharacterClass {
   KNIGHT, ENGINEER, THIEF, BLACK_MAGE, WHITE_MAGE, ENEMY
